e66b14443608a3052a53b42a8a9601c1617869f5
luidy <luidyc@gmail.com>
2024-11-03 14:10:28 -0300

    Modulo de ánalise finalizado

--- TreeApi/src/main/java/TreeApi/TreeApi/Service/NodeServiceInterface.java
+++ TreeApi/src/main/java/TreeApi/TreeApi/Service/NodeServiceInterface.java
@@ -3,14 +3,14 @@ package TreeApi.TreeApi.Service;
 import TreeApi.TreeApi.Entity.Node;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface NodeServiceInterface {
     Node createNode(Node request);
     Optional<Node> getById(Long id);
-    void addChild(Node child);
-    int getDepth();
+    int getDepth(Node node);
     Node searchByRoot(Node current, String rootName);
     Node searchByParentAtDepth(Node current, int depth);
-    List<Node> getAll();
+    Map<String,Integer> analyzeTree(Node tree, List<String> words,int depth);
 }
